7ca4d593647329d2e4bb2f2fae848518e7eab64d
aitoriasdev <aitoriasdev@gmail.com>
2025-11-26 19:06:33 +0000

    feature: Improved and fixed filterPatient function

--- frontend/src/utils/filterPatients.ts
+++ frontend/src/utils/filterPatients.ts
@@ -1,21 +1,10 @@
 import type {Patient} from './types/patient.ts';
 
 export function filterPatients(
-  searchText: string,
   patients: Patient[],
   acceptedFilter: 'all' | 'accepted' | 'refused' = 'all'
 ) {
   let filteredPatients = patients;
-
-  if (searchText) {
-    filteredPatients = filteredPatients.filter((patient) => {
-      return (
-        patient.name.toLowerCase().includes(searchText.toLowerCase()) ||
-        patient.dob.includes(searchText) ||
-        patient.gms.toLowerCase().includes(searchText.toLowerCase())
-      );
-    });
-  }
 
   if (acceptedFilter === 'accepted') {
     filteredPatients = filteredPatients.filter(
