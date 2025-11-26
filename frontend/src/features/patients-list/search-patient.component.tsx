94fd49ff508445a4a7bb258045027672d8abcfaf
aitoriasdev <aitoriasdev@gmail.com>
2025-11-26 19:35:30 +0000

    feature: Improved searchpatientcomponent

--- frontend/src/features/patients-list/search-patient.component.tsx
+++ frontend/src/features/patients-list/search-patient.component.tsx
@@ -1,24 +1,25 @@
-import type {ChangeEvent} from 'react';
+import type { ChangeEvent } from 'react';
+import { SVGComponent } from '../ui/svg.component.tsx';
 
 interface SearchPatientProps {
   inputSearchID: string;
   placeholder?: string;
+  value: string;
   onTextChange?: (text: string) => void;
 }
 
 export function SearchPatientComponent({
   inputSearchID,
   placeholder,
-  onTextChange = () => {},
+  value,
+  onTextChange = () => { },
 }: SearchPatientProps) {
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const inputText = event.target.value;
-
-    onTextChange(inputText);
+    onTextChange(event.target.value);
   };
 
   return (
-    <section className="patient-finder grid w-full grid-cols-1 pb-6">
+    <section className="patient-finder grid w-full grid-cols-1">
       <label htmlFor={inputSearchID} className="sr-only">
         Search patient
       </label>
@@ -27,25 +28,15 @@ export function SearchPatientComponent({
         id={inputSearchID}
         name={inputSearchID}
         placeholder={placeholder}
+        value={value}
         className="col-start-1 row-start-1 block w-full rounded-xl bg-white px-3 py-5 pr-3 pl-10 shadow-md outline-1 outline-offset-1 outline-gray-200 transition duration-200 ease-in-out focus:outline-4 focus:outline-offset-1 focus:outline-sky-500 dark:bg-white/5 dark:text-gray-400 dark:outline-gray-600"
         onChange={handleInputChange}
       />
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="col-start-1 row-start-1 ml-2 self-center text-gray-400"
-      >
+      <SVGComponent fill='none' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' className="col-start-1 row-start-1 ml-2 self-center text-gray-400">
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
         <path d="M21 21l-6 -6" />
-      </svg>
+      </SVGComponent>
     </section>
   );
 }
