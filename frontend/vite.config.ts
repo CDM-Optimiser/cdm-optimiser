aeee467cbb69da8053e6d61a120bfab74c1b77b9
aitoriasdev <aitoriasdev@gmail.com>
2025-11-30 12:50:18 +0000

    feature: Add vitest as dependency for testing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import path from 'path';
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
@@ -5,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
@@ -14,5 +15,10 @@ export default defineConfig({
       input: path.resolve(__dirname, 'index.html'),
     },
   },
-  base: './',
+  plugins: [react(), tailwindcss()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    include: ['src/test/**/*.test.{ts,tsx}'],
+  },
 });
