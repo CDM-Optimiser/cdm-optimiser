ba3e92ba2f8fc68943f8ec32ada6d06431f69d16
aitoriasdev <aitoriasdev@gmail.com>
2025-12-05 20:48:43 +0000

    fixes: Project fixes

--- main.js
+++ main.js
@@ -1,64 +1,64 @@
-import { app, BrowserWindow, ipcMain, nativeTheme } from 'electron/main';
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import {app, BrowserWindow, ipcMain, nativeTheme} from 'electron/main';
+import {dirname, join} from 'node:path';
+import {fileURLToPath} from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 function createWindow() {
-	const win = new BrowserWindow({
-		width: 1200,
-		height: 800,
-		webPreferences: {
-			contextIsolation: true,
-			nodeIntegration: false,
-			preload: join(__dirname, 'preload.js'),
-		},
-	});
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      preload: join(__dirname, 'preload.js'),
+    },
+  });
 
-	win.removeMenu();
+  win.removeMenu();
 
-	win.webContents.openDevTools();
+  win.webContents.openDevTools();
 
-	const indexPath = join(app.getAppPath(), 'frontend/dist/index.html');
-	win.loadFile(join(indexPath));
+  const indexPath = join(app.getAppPath(), 'frontend/dist/index.html');
+  win.loadFile(join(indexPath));
 }
 
 ipcMain.handle('dark-mode:get-source', () => {
-	return nativeTheme.themeSource;
+  return nativeTheme.themeSource;
 });
 
 ipcMain.handle('dark-mode:set', (event, mode) => {
-	nativeTheme.themeSource = mode;
+  nativeTheme.themeSource = mode;
 
-	return true;
+  return true;
 });
 
 ipcMain.handle('dark-mode:system', () => {
-	nativeTheme.themeSource = 'system';
+  nativeTheme.themeSource = 'system';
 });
 
 ipcMain.handle('dark-mode:toggle', () => {
-	if (nativeTheme.shouldUseDarkColors) {
-		nativeTheme.themeSource = 'light';
-	} else {
-		nativeTheme.themeSource = 'dark';
-	}
-	return nativeTheme.shouldUseDarkColors;
+  if (nativeTheme.shouldUseDarkColors) {
+    nativeTheme.themeSource = 'light';
+  } else {
+    nativeTheme.themeSource = 'dark';
+  }
+  return nativeTheme.shouldUseDarkColors;
 });
 
 app.whenReady().then(() => {
-	createWindow();
+  createWindow();
 
-	app.on('activate', () => {
-		if (BrowserWindow.getAllWindows().length === 0) {
-			createWindow();
-		}
-	});
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 });
 
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
